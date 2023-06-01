8986045ab52452e1f28010acb273dea1567b6458
nvsang260720 <nvsang2670@gmail.com>
2023-06-01 08:33:55 +0700

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="google-site-verification" content="hBKCTma5IGl6el9qy4ySelxwN4jw0nhjNWNXAS-c7iI" />
+      </Head>
       <body>
         <Script id="theme-switcher" strategy="beforeInteractive">
           {`if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
